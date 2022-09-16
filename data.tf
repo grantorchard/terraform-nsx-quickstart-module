614cd3e09d35b1913519305c7fda7bd2f6b2a7dc
Grant Orchard <go@hashicorp.com>
2022-09-16 14:33:14 +1000

    fix data source name

--- data.tf
+++ data.tf
@@ -6,6 +6,6 @@ data "nsxt_policy_edge_cluster" "this" {
   display_name = lookup(var.nsx_data[var.environment],"edge_cluster_name")
 }
 
-data "nsxt_policy_tier0_gateway" "tier0_gw_gateway" {
+data "nsxt_policy_tier0_gateway" "this" {
   display_name = lookup(var.nsx_data[var.environment],"tier0_name")
 }
