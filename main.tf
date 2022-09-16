0f8f1146b07ca45e6515b1a7d188c63d1e22a201
Grant Orchard <go@hashicorp.com>
2022-09-16 13:41:12 +1000

    query data sources for values rather than going via external workspace

--- main.tf
+++ main.tf
@@ -6,18 +6,18 @@ resource random_pet "this" {
   length = 2
 }
 
-module nsx_data {
-  source      = "app.terraform.io/grantorchard/remote-state/nsx"
-  version     = "0.0.1"
-  environment = lower(var.environment)
-}
+# module nsx_data {
+#   source      = "app.terraform.io/grantorchard/remote-state/nsx"
+#   version     = "0.0.1"
+#   environment = lower(var.environment)
+# }
 
 resource nsxt_policy_segment "public" {
   count               = length(var.public_subnets)
   display_name        = "${local.prefix}-${var.public_subnet_suffix}-${count.index}"
   description         = var.description
   connectivity_path   = nsxt_policy_tier1_gateway.this.path
-  transport_zone_path = module.nsx_data.transport_zone_path
+  transport_zone_path = data.nsxt_policy_transport_zone.this.path
   subnet {
     cidr = format("%s%s%s",
       cidrhost(element(var.public_subnets, count.index), 1),
@@ -35,7 +35,7 @@ resource nsxt_policy_segment "private" {
   display_name        = "${local.prefix}-${var.private_subnet_suffix}-${count.index}"
   description         = var.description
   connectivity_path   = nsxt_policy_tier1_gateway.this.path
-  transport_zone_path = module.nsx_data.transport_zone_path
+  transport_zone_path = data.nsxt_policy_transport_zone.this.path
   subnet {
     cidr = format("%s%s%s",
       cidrhost(element(var.private_subnets, count.index), 1),
@@ -51,13 +51,13 @@ resource nsxt_policy_segment "private" {
 resource nsxt_policy_tier1_gateway "this" {
   description               = var.description
   display_name              = "${local.prefix}-gateway"
-  edge_cluster_path         = module.nsx_data.edge_cluster_path
+  edge_cluster_path         = data.nsxt_policy_edge_cluster.this.path
   failover_mode             = "PREEMPTIVE"
   default_rule_logging      = "false"
   enable_firewall           = "false"
   enable_standby_relocation = "false"
   force_whitelisting        = "true"
-  tier0_path                = module.nsx_data.tier0_path
+  tier0_path                = data.nsxt_policy_tier0_gateway.this.path
   pool_allocation           = "ROUTING"
 
   route_advertisement_rule {
